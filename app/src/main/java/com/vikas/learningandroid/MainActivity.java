784863a7718ca9f650858b862ae30d435501ae2e
VikasNS <nsvikas1998@gmail.com>
2019-07-05 23:38:11 +0530

    [Location] Added location service using fused location provider

--- app/src/main/java/com/vikas/learningandroid/MainActivity.java
+++ app/src/main/java/com/vikas/learningandroid/MainActivity.java
@@ -1,13 +1,33 @@
 package com.vikas.learningandroid;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLocationClient ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fusedLocationClient  = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient .getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Toast.makeText(MainActivity.this,location.toString(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
